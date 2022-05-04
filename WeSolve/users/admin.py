eccff7e010ec828f03c681dc52c94a1873ca78a8
nirswartz <62308517+nirswartz@users.noreply.github.com>
2022-05-04 12:14:14 +0300

    users model

--- WeSolve/users/admin.py
+++ WeSolve/users/admin.py
@@ -7,7 +7,7 @@ class CustomUserAdmin(UserAdmin):
     # add_form = 
     # form = 
     model = CustomUser
-    list_display = ["username", "email", "is_staff"]
+    list_display = ["username", "email", "is_staff", "userId"]
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- WeSolve/users/models.py
+++ WeSolve/users/models.py
@@ -1,7 +1,44 @@
+import uuid
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Course(models.Model):
+    courseId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    courseName = models.CharField(max_length=50, unique=True)
+    courseNumber = models.CharField(max_length=50, unique=True)
+    # participants attribute is ManyToManyField relation to Users
+
+    def __str__(self):
+        return f'{self.courseNumber}_{self.courseName}'
+
+    class Meta:
+        db_table = 'Courses'
+
+
 class CustomUser(AbstractUser):
-    pass
+    """
+    Extends AbstractUser with users data for site's features.
+    Users data for authentication is saved under Django's default Users table:
+    username (same as here), first_name, last_name, email, password (hash only), date_joined
+    """ 
+    class Rank(models.IntegerChoices):
+        FRESHMAN = 0
+        JUNIOR = 1
+        SENIOR = 2
+
+    def upload_location(self, filename):
+        suffix = (filename.split('.'))[len(filename.split('.')) - 1]
+        return f'/uploads/userPics/{self.username}_userPic.{suffix}'
+
+    userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    courses = models.ManyToManyField(Course, related_name='participants', blank=True) # userCourses relation
+    rank = models.IntegerField(choices=Rank.choices, default=Rank.FRESHMAN)
+    userPic = models.ImageField(upload_to=upload_location, default="/uploads/userPics/default_userPic.png")
+    isTeacher = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
     
 
