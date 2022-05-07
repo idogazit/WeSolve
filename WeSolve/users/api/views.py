a2409ac0dc774a8f1022f6b3dbfc5de59fbec494
guysh95 <shnaiderguy@gmail.com>
2022-05-07 21:21:26 +0300

    schools api

--- WeSolve/users/api/views.py
+++ WeSolve/users/api/views.py
@@ -1,10 +1,46 @@
+from queue import Empty
 from requests import request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics, mixins, viewsets
 from rest_framework import permissions
-from users.models import CustomUser
-from users.api.serializers import UserDisplaySerializer, UserAdminDisplaySerializer
+from users.models import CustomUser, Faculty, School, Course
+from users.api.serializers import (UserDisplaySerializer, 
+                                   UserAdminDisplaySerializer,
+                                   FacultyListSerializer,
+                                   SchoolListSerializer)
+
+
+class FacultyListAPIView(generics.ListAPIView):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultyListSerializer
+
+
+class SchoolListAPIView(generics.ListAPIView):
+    serializer_class = SchoolListSerializer
+    # queryset = School.objects.all()
+
+    def get_queryset(self):
+        queryset = School.objects.all()
+        faculty = self.request.query_params.get("faculty")
+        print(faculty)
+        assert faculty is not None , (
+            'faculty argument missing'
+        )
+        if faculty is not None:
+            queryset = queryset.filter(facultyName=faculty)
+        assert queryset , (
+            '"%s" faculty dosent have any schools in our website' % faculty
+        )
+        return queryset
+    
+    # def get(self, request, *args, **kwargs):
+    #     faculty = self.request.query_params.get("faculty")
+    #     print(faculty)
+    #     return super().get(request, *args, **kwargs)
+    
+    
+
 
 
 class UserListAPIView(generics.ListAPIView):
