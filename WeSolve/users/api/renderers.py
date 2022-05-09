096d4e766bb2d12ece1631c66db33b6c4ccdbf8e
guysh95 <shnaiderguy@gmail.com>
2022-05-09 20:20:00 +0300

    added course api and modified faculties api

--- WeSolve/users/api/renderers.py
+++ WeSolve/users/api/renderers.py
@@ -1,7 +1,21 @@
 from rest_framework.renderers import JSONRenderer
 
 
-class myRenderer(JSONRenderer):
+class facultyRenderer(JSONRenderer):
+
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        if data is None:
+            return b''
+        
+        if "results" in data:
+            if "facultyName" in data["results"][0]:
+                for i in range(len(data["results"])):
+                    data["results"][i] = data["results"][i]["facultyName"]
+        
+        return super().render(data, accepted_media_type, renderer_context)
+
+class SchoolRenderer(JSONRenderer):
 
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
@@ -15,3 +29,17 @@ class myRenderer(JSONRenderer):
         
         return super().render(data, accepted_media_type, renderer_context)
 
+
+class CourseRenderer(JSONRenderer):
+
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        if data is None:
+            return b''
+        
+        if "results" in data:
+            if "courseName" in data["results"][0]:
+                for i in range(len(data["results"])):
+                    data["results"][i] = data["results"][i]["courseName"]
+        
+        return super().render(data, accepted_media_type, renderer_context)
